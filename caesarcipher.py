d584708c9a99c354d318b2681477103448c8bbbc
Tom Randle <tom.randle@gmail.com>
2012-08-15 17:30:52 +0100

    Improved variable names

--- caesarcipher.py
+++ caesarcipher.py
@@ -1,33 +1,38 @@
+clearText = 'Prince Andrew, seeing that his father insisted, began- at first reluctantly, but gradually with more and more animation, and from habit changing unconsciously from Russian to French as he went on- to explain the plan of operation for the coming campaign. He explained how an army, ninety thousand strong, was to threaten Prussia so as to bring her out of her neutrality and draw her into the war; how part of that army was to join some Swedish forces at Stralsund; how two hundred and twenty thousand Austrians, with a hundred thousand Russians, were to operate in Italy and on the Rhine; how fifty' 
+cipherText = ""
 
+key = 2 #Caesar shift offset
+textBlockSize = 5 #How to group letters
+lettersRemoved = 0
 
-rawtext = 'Prince Andrew, seeing that his father insisted, began- at first reluctantly, but gradually with more and more animation, and from habit changing unconsciously from Russian to French as he went on- to explain the plan of operation for the coming campaign. He explained how an army, ninety thousand strong, was to threaten Prussia so as to bring her out of her neutrality and draw her into the war; how part of that army was to join some Swedish forces at Stralsund; how two hundred and twenty thousand Austrians, with a hundred thousand Russians, were to operate in Italy and on the Rhine; how fifty' 
+upperclearText = clearText.upper()
 
-uppertext = rawtext.upper()
-print uppertext
+for index, ch in enumerate(upperclearText):
 
-key = 2
-codedMessage = ""
-textblocksize = 5
+	clearAsciiCharacter =  ord(ch)
 
-for index, ch in enumerate(uppertext):
+	if (90 - 26) < clearAsciiCharacter <= 90 :
+		cipherAsciiCharacter = clearAsciiCharacter + key
 
-	asciicharacter =  ord(ch)
+		if cipherAsciiCharacter > 90:
+			cipherAsciiCharacter -= 26
+		
+		cipherCharacter = chr(cipherAsciiCharacter)
 
-	if (90 - 26) < asciicharacter <= 90 :
-		asciicodedcharacter = asciicharacter + key
-
-		if asciicodedcharacter > 90:
-			asciicodedcharacter -= 26
-
-		codedcharacter = chr(asciicodedcharacter)
+		if (index - lettersRemoved) % textBlockSize == textBlockSize - 1:
+			cipherCharacter +=" "
 
 	else:
-		codedcharacter = "" 
+		cipherCharacter = ""
+		lettersRemoved += 1
 
-	
-	if index % textblocksize == 0:
-		codedcharacter += " "
-	
-	codedMessage += codedcharacter
+	cipherText += cipherCharacter
 
-print "The coded message is:", codedMessage
+print "*" * 20
+print "CLEAR TEXT"
+print "*" * 20
+print upperclearText
+print "*" * 20
+print "CIPHER TEXT"
+print "*" * 20
+print cipherText
